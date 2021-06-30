72042c5f54d763ba825321b35e52e55f38a26007
nazarko310 <nazar2000pok@gmail.com>
2021-06-30 21:14:26 +0300

    lessons2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import {useEffect, useState} from "react";
-import Users from "./components/users/Users";
 
 function App() {
-    let [postsList, setPostsList] = useState([]);
-    useEffect(() => {
-
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(value => value.json())
-            .then(response => {
-                setPostsList(response)
-            });
-    }, [])
-    return (
-        <div>
-            {
-                <Users items={postsList}/>
-
-            }
-        </div>
-    )
 }
-
 export default App;
 
