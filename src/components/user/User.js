d1cf667307e7d1a33d9bba7ac98abd64af657f15
nazarko310 <nazar2000pok@gmail.com>
2021-07-04 13:43:30 +0300

    lessons3-practice2

--- src/components/user/User.js
+++ src/components/user/User.js
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export default function User({item, url}) {
     return (
         <div>
-            {item.id} - {item.name}
+            {item.id} - {item.name} - {item.username}
             <Link to={`${url}/${item.id}/albums`}>Albums</Link>
         </div>
     )
